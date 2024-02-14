29bbb63031d52abaf9e73e8264cca8b7e26cb1e9
Evan Gantert <egantert@udel.edu>
2024-02-13 20:37:26 -0500

    Changed name text slightly

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Evan Gantert
+                UD CISC275 with React Hooks and TypeScript : Evan Gantert
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
